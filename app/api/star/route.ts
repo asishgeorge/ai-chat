9df9e5800cfdf59866d464079f65f7d7deff32c6
Asish George <asishag12@gmail.com>
2025-02-24 02:08:30 +0530

    logging errors

--- app/api/star/route.ts
+++ app/api/star/route.ts
@@ -52,7 +52,7 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ success: true });
   } catch (error) {
-   
+    console.log('error', error);
     return NextResponse.json(
       { error: "Failed to update message star status" },
       { status: 500 }
